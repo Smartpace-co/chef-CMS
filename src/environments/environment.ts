a95aeba0ce96bc3a271c58e3452b1822a9349dab
Incipient <layla@chefkoochooloo.com>
2021-10-13 14:34:05 +0530

    Fix:env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
-	production: true,
-	assetUrl: 'https://api-core-stage.chefkoochooloo.com/',
-	apiBaseUrl: 'https://api-core-stage.chefkoochooloo.com/api/v1',
-	cmsApiBaseUrl: 'https://api-cms-stage.chefkoochooloo.com/api/v1',
+	production: false,
+	appVersion: 'v717demo1',
+	USERDATA_KEY: 'authf649fc9a5f55',
+	isMockEnabled: true,
+	apiUrl: 'https://api-cms-stage.chefkoochooloo.com/api/v1',
+	chefkCoreApi: 'https://api-core-stage.chefkoochooloo.com/api/v1',
 	public_key:'pk_test_51J2qQTLnyrqVEiSe5LUumXNARXUbszlstNit2HsGbPr2rRtJPSttDyGHEOt4yIU4M14e2KRz3f1Oa53sNhDOyMTv00Hun3pjbK',
 };
